9e468c28a84abf1b65f15e5e56c1531b1d4aa9fc
LZHD <lzhdfxq@163.com>
2020-08-22 18:16:49 +0800

    chore: update to angular9.0

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-picture-viewer!');
+    expect(page.getTitleText()).toEqual('ng-picture-viewer app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CommonModule, registerLocaleData} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
-import zh from '@angular/common/locales/zh';
-import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
-import {ImgViewerModule} from 'ng-picture-viewer/src/img-viewer.module';
-
-registerLocaleData(zh);
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ImgViewerModule } from 'ng-picture-viewer';
 
 @NgModule({
   declarations: [
@@ -18,11 +14,9 @@ registerLocaleData(zh);
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
-    NgZorroAntdModule,
     HttpClientModule,
     ImgViewerModule
   ],
-  providers: [{provide: NZ_I18N, useValue: zh_CN}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
