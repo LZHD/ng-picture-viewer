6b98b2525970de3efd66858a29b6d96121ff727c
LZHD <lzhdfxq@163.com>
2021-02-04 20:53:55 +0800

    chore: update to angular10

--- lib/src/img-viewer.module.ts
+++ lib/src/img-viewer.module.ts
@@ -2,7 +2,8 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImgViewerComponent } from './img-viewer.component';
 import { ImgViewerConfig } from './img-viewer.config';
-import { NzIconModule, NzToolTipModule } from 'ng-zorro-antd';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { NzIconModule, NzToolTipModule } from 'ng-zorro-antd';
   ]
 })
 export class ImgViewerModule {
-  static forRoot(config?: ImgViewerConfig): ModuleWithProviders {
+  static forRoot(config?: ImgViewerConfig): ModuleWithProviders<ImgViewerModule> {
     return {
       ngModule: ImgViewerModule,
       providers: [{provide: ImgViewerConfig, useValue: config}]
